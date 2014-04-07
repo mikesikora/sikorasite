67467eeb5fc597a88523227601eb3f65a0f8a316
Michael Sikora <msikora.umd@gmail.com>
2014-04-07 01:00:35 -0400

    built body structure

--- index.php
+++ index.php
@@ -1,5 +1,95 @@
 <?php include_once('partials/header.html') ?>
 
-	<div></div>
+	<!-- Header Style -->
+
+	<header>
+		<div class="container">
+			<div class="brand">
+				<h1>Michael Sikora</h1>
+			</div>
+			<ul class="nav-bar ">
+				<li><a href="#about">About</a></li>
+				<li><a href="#projects">Projects</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- Featured Header Area (backdrop) -->
+
+	<div class="header-container">
+		<div class="header-overlay"></div>
+		<div class="header-featured-area">
+			<div class="header-blurb">
+				<img src="http://i.imgur.com/8G8v7Mf.png">
+				<h1>You Fail When You Quit.</h1>
+			</div>
+			<button>Learn More</button>
+		</div>
+	</div>
+
+	<!-- Main Body Contain -->
+
+	<div class="body-container">
+
+		<div id="about" class="content"> <!-- Start of About Section -->
+			<div class="container"> <!-- Start of .container -->
+				<h1>About</h1>
+				<p>Taking risks, learning something new, and having fun are words of wisdom that have been told to me since I was a child. I try to live by those three pillars of ideology, and I realize that every opportunity has the chance to impact my life.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+			</div>
+		</div>
+
+		<div id="projects" class="content"> <!-- Start of Projects Section -->
+			<div class="container"> <!-- Start of .container -->
+				<h1>Projects</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+			</div>
+		</div>
+
+		<div id="contact" class="content"> <!-- Start of Projects Section -->
+			<div class="container"> <!-- Start of .container -->
+				<h1>Contact</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	
+			</div>
+		</div>
+
+	</div> <!-- Close Body Container -->
 
 <?php include_once('partials/footer.html') ?>
