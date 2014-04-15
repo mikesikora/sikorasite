c46226ecbbaf378c80d2d0e7b13fa9f4bfa781f8
Michael Sikora <msikora.umd@gmail.com>
2014-04-15 15:55:58 -0400

    Added content and style

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 		<div id="about" class="content"> <!-- Start of About Section -->
 			<div class="container"> <!-- Start of .container -->
 				<div class="row clear-fix">
-					<div class="column-2">
+					<div class="column-2" style="padding:30px 0;">
 						<h1 class="color-red">I'm an inspired 21 year old Mechanical Engineer. I love to learn and create.</h1>
 						<p>I am a coder. Each language I learn is a tool to bring my ideas to life. I've learned to do some pretty cool things, and I'll share them with you.</p>
 					</div>
@@ -136,7 +136,14 @@
 					</div>
 				</div>
 				<div class="row clear-fix">
-					<p>I also have used a suite of engineering and design software. I have used Matlab and Simulink, Solidworks, Autodesk Inventor, and Pro E. For design I have used Adobe Photoshop, Illustrator, and After Effects.</p>
+						<img src="images/mouse.png" style="width:300px; padding-right:20px;" class="push-left">
+						<div style="padding: 50px 0;">
+							<h1>Software</h1>
+							<p>Through my education and hobbies I have used a host of amazing software:<br>
+							<strong>Solidworks</strong> | <strong>Autodesk Inventor</strong> | <strong>Pre E (PTC CREO)</strong> | <strong>Matlab</strong> | <strong>Simulink</strong><br>
+							<strong>Adobe Photoshop</strong> | <strong>Adobe Illustrator</strong> | <strong>Adobe After Effects</strong> | <strong>Eclipse</strong>
+							</p>
+						</div>
 				</div>
 			</div>
 		</div>
@@ -145,14 +152,11 @@
 		<div id="resume" class="content"> <!-- Start of About Section -->
 			<div class="container"> <!-- Start of .container -->
 				<div class="row clear-fix">
-					<h1>Resume</h1>
-
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<div class="center-text">
+						<h1>Resume</h1>
+						<p>Select one of the links below to download a PDF version of my resume.</p>
+						<a href="#" class="download-resume">Traditional </a> <a href="#" class="download-resume">Modern</a>
+					</div>
 				</div>
 			</div>	
 		</div>
@@ -167,17 +171,9 @@
 					
 					<div class="portfolio-item">
 						<figure>
-							<div class="info"><a href="#" class="info">&#10162; Hello World</a></div>
+							<div class="info"><a href="#" class="info">&#10162; Twitterplays.com</a></div>
 							<div class="info-backdrop"></div>
-							<img src="http://i.imgur.com/xb6MC.jpg">
-						</figure>
-					</div>
-
-					<div class="portfolio-item">
-						<figure>
-							<div class="info"><a href="#" class="info">&#10162; Snowboard</a></div>
-							<div class="info-backdrop"></div>
-							<img src="http://xtoutfitters.eu/wp-content/uploads/2013/02/url-2.jpeg">
+							<img src="http://i.imgur.com/J9uxMud.png">
 						</figure>
 					</div>
 
@@ -191,17 +187,33 @@
 
 					<div class="portfolio-item">
 						<figure>
-							<div class="info"><a href="#" class="info">&#10162; Another Project</a></div>
+							<div class="info"><a href="#" class="info">&#10162; A 3D Print</a></div>
 							<div class="info-backdrop"></div>
-							<img src="http://i.imgur.com/aGSQwZB.png">
+							<img src="http://i.imgur.com/1QKH8OB.png">
 						</figure>
 					</div>
 
 					<div class="portfolio-item">
 						<figure>
-							<div class="info"><a href="#" class="info">&#10162; Projects Again</a></div>
+							<div class="info"><a href="#" class="info">&#10162; Micro Cantilevers</a></div>
 							<div class="info-backdrop"></div>
-							<img src="http://www.techonshow.com/wp-content/uploads/2013/10/Twitter-logo.jpg">
+							<img src="http://i.imgur.com/MjF2qrd.jpg">
+						</figure>
+					</div>
+
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; NCAA Bracket</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/x54Ewwd.png">
+						</figure>
+					</div>
+
+					<div class="col-3 portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Hello</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/Md6pkSn.jpg">
 						</figure>
 					</div>
 
@@ -214,28 +226,18 @@
 					</div>
 
 				</div>
-
-				<p>Hello World</p>
-
 			</div>
 		</div>
 
 		<div id="contact" class="content"> <!-- Start of Projects Section -->
 			<div class="container"> <!-- Start of .container -->
-				<h1>Contact</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	
+				<div class="center-text">
+					<h1>Contact</h1>
+					<p>email: msikora.umd@gmail.com</p>
+					<p>phone: (xxx) xxx-xxxx</p>
+					<p>Linkdin</p>
+					<p>Github</p>
+				</div>
 			</div>
 		</div>
 
