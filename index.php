a20e486b7754ef63950b37c324a79c333bee5375
Michael Sikora <msikora.umd@gmail.com>
2014-04-15 02:16:02 -0400

    Updated structure

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 				<img src="http://i.imgur.com/8G8v7Mf.png">
 				<h1>You Fail When You Quit.</h1>
 			</div>
-			<button href="#about" class="nav-link">Learn More</button>
+			<button href="#about" class="nav-link learn-more-button">Learn More</button>
 		</div>
 	</div>
 
@@ -135,82 +135,88 @@
 						<!-- <p class="display-grid-sm-detail push-right">I am a coder. Each language I learn is a tool to bring my ideas to life. I've learned to do some pretty cool things, and I'll share them with you.</p> -->
 					</div>
 				</div>
+				<div class="row clear-fix">
+					<p>I also have used a suite of engineering and design software. I have used Matlab and Simulink, Solidworks, Autodesk Inventor, and Pro E. For design I have used Adobe Photoshop, Illustrator, and After Effects.</p>
+				</div>
 			</div>
 		</div>
 
-		<div id="projects" class="content"> <!-- Start of Projects Section -->
+
+		<div id="resume" class="content"> <!-- Start of About Section -->
 			<div class="container"> <!-- Start of .container -->
-				<h1>Projects</h1>
-				<div id="porfolio" class="grid">
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
-					<div class="col-3 portfolio-item">
-						<figure>
-							<div class="info"></div>
-							<img src="">
-						</figure>
-					</div>
+				<div class="row clear-fix">
+					<h1>Resume</h1>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	
+			</div>	
+		</div>
+
+		<div id="projects" class="content"> <!-- Start of Projects Section -->
+			<div class="container clear-fix"> <!-- Start of .container -->
+				<div class="center-text">
+						<h1>Projects</h1>
+						<p>Web Design | 3D Design | Graphic Design</p>
+					</div>
+				<div id="portfolio" class="grid">
+					
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Hello World</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/xb6MC.jpg">
+						</figure>
+					</div>
+
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Snowboard</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://xtoutfitters.eu/wp-content/uploads/2013/02/url-2.jpeg">
+						</figure>
+					</div>
+
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Terpshell</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/nl6cVTX.jpg">
+						</figure>
+					</div>
+
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Another Project</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/aGSQwZB.png">
+						</figure>
+					</div>
+
+					<div class="portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Projects Again</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://www.techonshow.com/wp-content/uploads/2013/10/Twitter-logo.jpg">
+						</figure>
+					</div>
+
+					<div class="col-3 portfolio-item">
+						<figure>
+							<div class="info"><a href="#" class="info">&#10162; Hello</a></div>
+							<div class="info-backdrop"></div>
+							<img src="http://i.imgur.com/Md6pkSn.jpg">
+						</figure>
+					</div>
+
+				</div>
+
+				<p>Hello World</p>
+
 			</div>
 		</div>
 
--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ $(function() {
       }else{
       	$('header').removeClass('header-shrink');
       }
+      var aboutOffset = $("#about").offset().top;
+      if(scrolled>aboutOffset-70 && scrolled<aboutOffset){
+      	colorGridMove();
+      	console.log(aboutOffset);
+      }
    });
 
 	$(".nav-link").click(function(){
@@ -14,7 +19,25 @@ $(function() {
 		return false;
 	});
 
-	var randomColor = setInterval(colorGrid,3000);
+	//var randomColor = setInterval(colorGrid,3000);
+
+	function colorGridMove(){
+		var piece = $('.piece-back');
+		var pieceSize = piece.length;
+		for (var i = 0; i < pieceSize; i++) {
+			(function(i){
+				setTimeout(function(){
+					piece.eq(i).addClass('nograyscale img-scale');
+				}, 250*i);
+			});
+			(function(i){
+				setTimeout(function(){
+					piece.eq(i).removeClass('nograyscale img-scale');
+				}, 250*i);
+			});
+			
+		};
+	};
 
 	function colorGrid(){
 		var piece = $('.piece-back');
@@ -30,30 +53,46 @@ $(function() {
 	$(".grid-piece").hover(function(){
 		$(this).children('.piece-front').addClass('top-fix');
 		$(this).children('.piece-back').addClass('img-scale nograyscale');
-		clearInterval(randomColor);
+		// clearInterval(randomColor);
 	},function(){
 		$(this).children('.piece-front').removeClass('top-fix');
 		$(this).children('.piece-back').removeClass('img-scale nograyscale');
-		randomColor = setInterval(colorGrid,3000);
+		// randomColor = setInterval(colorGrid,3000);
 	});
 
-	function createPortfolio(){
+	function createPortfolio(Width, Height){
 		var port = $('.portfolio-item');
 		var counting = -1;
 		for (var i = 0; i < port.length; i++){
 		    if(i%3===0){
 		        counting++;
 		    }
-		    var newLeft = i%3*110+"px";
-		    var newTop = counting*110+"px";
+		    var newLeft = i%3*Width+"px";
+		    var newTop = counting*Height+"px";
 		    port.eq(i).css({
 		        "left":newLeft,
 		        "top":newTop
 		    });
 		}
+		$('#portfolio').css({
+			"height":(counting+1)*Height+"px",
+			"width":3*Width+"px"
+		});
 	}
 
-	createPortfolio();
+	createPortfolio(340,283);
+
+	$("figure").hover(function(){
+		$(this).children('img').addClass('portfolio-img-fix');
+		$(this).children('.info-backdrop').addClass('top-fix');
+		$(this).children('.info').addClass('top-fix-50');
+		console.log("maybe");
+	},function(){
+		$(this).children('img').removeClass('portfolio-img-fix');
+		$(this).children('.info-backdrop').delay(200).removeClass('top-fix');
+		$(this).children('.info').removeClass('top-fix-50');
+		console.log("okay");
+	});
 
 });
 
